a3dd41a6567c78e515602865cfb70d6c7a62270f
Lina Shao <Lingxiao.Shao@walmartlabs.com>
2018-08-02 22:01:07 -0700

    added parse

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -36,6 +37,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
+        Log.i("json", json);
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,44 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        JSONObject jo = null;
+        Sandwich sd = null;
+        try {
+            jo = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            String mainName = jo.getJSONObject("name").getString("mainName");
+            JSONArray aka = jo.getJSONObject("name").getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < aka.length(); i++) {
+                alsoKnownAs.add(aka.getString(i));
+            }
+            String placeOfOrigin = jo.getString("placeOfOrigin");
+            String description = jo.getString("description");
+            String image = jo.getString("image");
+            JSONArray in = jo.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < in.length(); i++) {
+                ingredients.add(in.getString(i));
+            }
+            sd = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sd;
+
     }
 }
